ccb7faa04ea6ffe8d4e81b3c79acb9f3ab803538
moritzw01 <moritzwendekamm@web.de>
2025-09-18 15:27:45 +0200

    inference method, that reads in a json file and outputs a json file with segmentation masks

--- inference.py
+++ inference.py
@@ -1,3 +1,25 @@
 """
 inference with sam 
 """
+
+from segment_anything import SamPredictor, sam_model_registry
+
+import json 
+
+def inference(js_file, model_checkpoint='<path/to/checkpoint>', model_type='',):
+    res = [] 
+    box_predictions = json.load(js_file)
+    sam = sam_model_registry[model_type](checkpoint=model_checkpoint)
+    predictor = SamPredictor(sam)
+    for pred in box_predictions:
+        predictor.set_image(pred['filename'])
+        masks, seg_scores, seg_logits = predictor.predict(pred['pred_boxes'])
+        res.append({'pred_masks':masks
+                    , 'pred_seg_scores':seg_scores
+                    , 'pred_seg_logits':seg_logits
+                    , 'filename': pred['filename']
+                    , 'pred_boxes': pred['pred_boxes']
+                    , 'pred_box_scores': pred['pred_boxes']
+                    })
+
+
